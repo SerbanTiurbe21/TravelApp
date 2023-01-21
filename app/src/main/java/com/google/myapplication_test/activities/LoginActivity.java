6d0f98b985110b89c1059748fd8e8de3fe768e54
SerbanTiurbe21 <serban.tiurbe@student.upt.ro>
2023-01-21 09:59:22 +0200

    some updates

--- app/src/main/java/com/google/myapplication_test/activities/LoginActivity.java
+++ app/src/main/java/com/google/myapplication_test/activities/LoginActivity.java
@@ -95,7 +95,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         setupViews();
         registerButtonAction();
         setLoginButtonLogin();
